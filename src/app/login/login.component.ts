e113cf4d76457d4b8a57d6f0db0c7a5e3ecd5c02
Gareth A <garethjaustin@gmail.com>
2024-05-21 14:15:48 +0100

    adds environment variable

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AuthService } from '../auth.service'; // Adjust the path if necessary
+import { Observable } from 'rxjs';
+import firebase from 'firebase/compat/app'; // Ensure this import for compatibility
+
 @Component({
   selector: 'app-signin',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
-  constructor(public angularFireAuth: AngularFireAuth) {}
+export class LoginComponent implements OnInit {
+  user$!: Observable<firebase.User | null>; // Use firebase.User for correct typing
+
+  constructor(
+    public angularFireAuth: AngularFireAuth,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.user$ = this.authService.getUserInfo();
+  }
+
   logOut() {
     this.angularFireAuth.signOut();
   }
