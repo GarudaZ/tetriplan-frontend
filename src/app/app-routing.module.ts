8d547a0e093e68a24fa33ce2a1fd3b9f4c2a0c52
Gareth A <garethjaustin@gmail.com>
2024-05-22 16:56:25 +0100

    adds additional default  path and image to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home.component';
 import { authGuard } from './auth.guard';
 
 const routes: Routes = [
+  { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   {
     path: 'home',
