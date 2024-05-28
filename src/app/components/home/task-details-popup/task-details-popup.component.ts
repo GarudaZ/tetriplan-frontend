d604cc74b737d7cb705fb0899182bf56e6584372
Ladan Torkzaban <99889241+ladantork@users.noreply.github.com>
2024-05-28 12:36:58 +0100

    can now edit task and see the console.log changes but the task not getting updated

--- src/app/components/home/task-details-popup/task-details-popup.component.ts
+++ src/app/components/home/task-details-popup/task-details-popup.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Task } from '../../../services/task.service';
@@ -8,14 +9,30 @@ import { Task } from '../../../services/task.service';
   styleUrls: ['./task-details-popup.component.css'] 
 })
 export class TaskDetailsPopupComponent {
+    
+  isEditing = false;
+  editableTask: Task;
 
   constructor(
     public dialogRef: MatDialogRef<TaskDetailsPopupComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Task
-  ) {}
+  ) {
+
+    this.editableTask = { ...data }; // Make a copy of the task data
+  }
 
   closeDialog(): void {
     this.dialogRef.close();
+  }
+  toggleEdit(): void {
+    this.isEditing = !this.isEditing;
+  }
+
+  saveTask(): void {
+    // Save logic here
+    console.log('Task saved', this.editableTask);
+    this.isEditing = false;
+    // Optionally close the dialog or call a service to save the task
   }
 }
 
