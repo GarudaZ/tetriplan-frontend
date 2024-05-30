efca060675a135f7171e920ccf3acebe022ffc2a
Gareth A <garethjaustin@gmail.com>
2024-05-30 16:53:38 +0100

    adds routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,29 +5,24 @@ import { AnalyticsComponent } from './components/analytics/analytics.component';
 import { HomeComponent } from '../app/components/home/home.component';
 import { authGuard } from './services/auth.guard';
 import { RegisterComponent } from './components/register/register.component';
-import {ForgotPasswordComponent} from '../app/components/forgot-password/forgot-password.component' 
+import { ForgotPasswordComponent } from '../app/components/forgot-password/forgot-password.component';
 
 const routes: Routes = [
+  { path: '', component: LoginComponent },
 
-  { path: '', 
-   component: LoginComponent },
-  
-  { path: 'login',
-   component: LoginComponent },
+  { path: '/login', component: LoginComponent },
 
-  {path: 'register',
-   component : RegisterComponent},
+  { path: '/register', component: RegisterComponent },
 
-   {path: 'forgot-password', 
-   component : ForgotPasswordComponent},
+  { path: '/forgot-password', component: ForgotPasswordComponent },
 
   {
-    path: 'home',
+    path: '/home',
     component: HomeComponent,
     canActivate: [authGuard],
   },
   {
-    path: 'analytics',
+    path: '/analytics',
     component: AnalyticsComponent,
     canActivate: [authGuard],
   },
